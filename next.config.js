c1571559ad6423d829dffd6abf12e89abf67f5c7
asutoosh <asutoshpattanayak806@gmail.com>
2025-12-07 18:35:59 +0000

    pushing it 1

--- next.config.js
+++ next.config.js
@@ -5,12 +5,20 @@ const nextConfig = {
     // Ignore bot folder during build
     ignoreBuildErrors: false,
   },
+  // Exclude bot folder completely
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   // Exclude bot folder from webpack compilation
   webpack: (config, { isServer }) => {
+    // Ignore bot folder in webpack
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+    
     config.watchOptions = {
       ...config.watchOptions,
-      ignored: ['**/node_modules', '**/bot/**'],
+      ignored: ['**/node_modules', '**/bot/**', '**/.next/**'],
     }
+    
     return config
   },
 }
