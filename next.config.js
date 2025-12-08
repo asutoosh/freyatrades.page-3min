5d3849c94823cea6f1af630c73808f2b2ced3d87
asutoosh <asutoshpattanayak806@gmail.com>
2025-12-08 17:13:54 +0000

    lets try one more tryime

--- next.config.js
+++ next.config.js
@@ -9,9 +9,10 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   // Exclude bot folder from webpack compilation
   webpack: (config, { isServer }) => {
-    // Ignore bot folder in webpack
+    // Ensure @ alias is properly resolved
     config.resolve.alias = {
       ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
     }
     
     config.watchOptions = {
